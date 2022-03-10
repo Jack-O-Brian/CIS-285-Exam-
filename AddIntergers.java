a953507d1d29a02e6acdc06b5b1afb9f1aa3c711
Scotty <40522299+Jack-O-Brian@users.noreply.github.com>
2022-03-10 14:25:33 -0500

    Update AddIntergers.java

--- AddIntergers.java
+++ AddIntergers.java
@@ -2,12 +2,14 @@ public class AddIntergers {
 
     public static void main(String[] args) {
 
-        int first = 15;
-        int second = 25;
+        int first = 1555555;
+        int second = 255555555;
 
-        System.out.println("Enter two numbers: " + first + " " + second);
+        System.out.println("Enter two numbers CHANGED: " + first + " " + second);
         int sum = first + second;
 
-        System.out.println("The sum is: " + sum);
+        System.out.println("The sum is: " + 53);
+                System.out.println("The sum is: " + sum);
+
     }
 }
